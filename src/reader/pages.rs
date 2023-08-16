9257614bf35d273fe0791798d51310c36bb40de3
phant80m <black.pandazayd@gmail.com>
2023-08-16 16:39:54 +1000

    included the markdown files so you dont need them in path

--- src/reader/pages.rs
+++ src/reader/pages.rs
@@ -1,14 +1,11 @@
-use std::fs::read_to_string;
-
 use super::Pages;
 impl Pages {
     pub fn construct() -> Self {
+        let home: String = include_str!("../../pages/index.md").to_string();
+        let install: String = include_str!("../../pages/install.md").to_string();
+        let master_tutorial: String = include_str!("../../pages/master.md").to_string();
         // in order
-        let pages = vec![
-            read_to_string("pages/index.md").unwrap_or_default(),
-            read_to_string("pages/install.md").unwrap_or_default(),
-            read_to_string("pages/master.md").unwrap_or_default(),
-        ];
+        let pages = vec![home, install, master_tutorial];
         Self {
             current_page: 0,
             pages,
